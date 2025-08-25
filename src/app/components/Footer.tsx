860ec3a4e2af521f428b44e5bff44949d35f9131
nihil404 <peacemakerallones@gmail.com>
2025-08-26 03:36:52 +0800

    centered the text

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,9 +5,10 @@ export default function Footer() {
       
 <div className="flex flex-col items-center justify-center w-full mt-8 space-y-8">
   <hr className="w-full border-t border-gray-600" />
-  <p className="text-sm text-gray-600 mb-6">
+  <p className="text-sm text-gray-600 mb-6 text-center">
     © 2025 Regeryl Lumbayan. Built with <span className="font-semibold">Next.js</span> & <span className="font-semibold">Tailwind CSS</span>.
   </p>
 </div>
+
 
     )}
